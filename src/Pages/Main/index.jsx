5155f9723f1a9e57ab0412e908d686bf557a495c
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-02 17:23:59 -0400

    :sparkles: Hello, world

---
+++ src/Pages/Main/index.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+
+function Main() {
+  return <h1>Hello, World</h1>;
+}
+
+export default Main;
---
+++ src/Pages/Repository/index.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+
+function Repository() {
+  return <h1>Hello, World</h1>;
+}
+
+export default Repository;
