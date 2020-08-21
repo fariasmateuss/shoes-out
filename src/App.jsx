985adad1bf2503614fce3eab68a115e209e1335a
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-20 22:23:40 -0400

    :art: Renamed path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes />
     </BrowserRouter>
   );
