0ae2d2bdbec728650990d2f820c16a24db476ea6
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-20 23:39:52 -0400

    :sparkles: Page header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Cart } from './styles';
+
+import { MdShoppingCart } from 'react-icons/md';
+
+function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <h1>Shoes Out</h1>
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>4 itens</span>
+        </div>
+        <MdShoppingCart size={36} />
+      </Cart>
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+  }
+`;
