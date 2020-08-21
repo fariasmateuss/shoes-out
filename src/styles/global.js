4dfcfa2baa7a58f5482cd902bae640ed30080f76
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-20 23:38:38 -0400

    :sparkles: Set text decoration and color default

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,10 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
 * {
+  color: white;
   margin: 0;
   padding: 0;
   outline: 0;
   box-sizing: border-box;
+  text-decoration: none;
 }
 
 body {
