4805974cbb8baab6cc5a4641c7ccf9dea8e6f205
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-21 23:24:21 -0400

    :sparkles: Initial product card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import { ProductList } from './styles';
+
 function Home() {
-  return <h1>Hello, World</h1>;
+  return (
+    <ProductList>
+      <div class="container">
+        <div class="card">
+          <div class="imgBx">
+            <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
+          </div>
+          <div class="contentBx">
+            <h2>Vans Shoe</h2>
+            <div class="size">
+              <h3>Size :</h3>
+              <span>37</span>
+              <span>38</span>
+              <span>39</span>
+              <span>40</span>
+            </div>
+            <div class="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <a href="#">Buy Now</a>
+          </div>
+        </div>
+      </div>
+    </ProductList>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,180 @@
+import styled from 'styled-components';
+
+export const ProductList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+
+  .container {
+    position: relative;
+
+    .card {
+      position: relative;
+      width: 320px;
+      height: 450px;
+      background: #232323;
+      border-radius: 20px;
+      overflow: hidden;
+
+      &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #eab15b;
+        clip-path: circle(150px at 80% 20%);
+        transition: 0.5s ease-in-out;
+      }
+
+      &:hover:before {
+        clip-path: circle(300px at 80% -20%);
+      }
+
+      &:after {
+        content: 'Vans';
+        position: absolute;
+        top: 30%;
+        left: -30px;
+        font-size: 9em;
+        font-weight: 800;
+        font-style: italic;
+        color: rgba(255, 255, 255, 0.04);
+      }
+
+      .imgBx {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50px);
+        z-index: 10000;
+        width: 100%;
+        height: 220px;
+        transition: 1s;
+
+        img {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%) rotate(-25deg);
+          width: 270px;
+        }
+      }
+
+      &:hover .imgBx {
+        top: 0%;
+        transform: translateY(0%);
+      }
+
+      &:hover .contentBx {
+        height: 210px;
+      }
+
+      .contentBx {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 100px;
+        text-align: center;
+        transition: 1s;
+        z-index: 10;
+
+        h2 {
+          position: relative;
+          font-weight: 600;
+          letter-spacing: 1px;
+          padding-top: 30px;
+          color: #fff;
+        }
+
+        .size,
+        .color {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 8px 20px;
+          transition: 0.5s;
+          opacity: 0;
+          visibility: hidden;
+
+          h3 {
+            color: #fff;
+            font-weight: 300;
+            font-size: 14px;
+            text-transform: uppercase;
+            letter-spacing: 2px;
+            margin-right: 10px;
+          }
+        }
+
+        .size span {
+          width: 26px;
+          height: 26px;
+          text-align: center;
+          line-height: 26px;
+          font-size: 14px;
+          display: inline-block;
+          color: #111;
+          background: #fff;
+          margin: 0 5px;
+          transition: 0.5s;
+          color: #111;
+          border-radius: 4px;
+          cursor: pointer;
+
+          &:hover {
+            background: #eab15b;
+          }
+        }
+
+        .color span {
+          width: 20px;
+          height: 20px;
+          background: #ff0;
+          border-radius: 50%;
+          margin: 0 5px;
+          cursor: pointer;
+
+          &:nth-child(1) {
+            background: #eab15b;
+          }
+
+          &:nth-child(2) {
+            background: #eab15b;
+          }
+
+          &:nth-child(3) {
+            background: #e91e63;
+          }
+        }
+
+        a {
+          display: inline-block;
+          padding: 10px 20px;
+          background: #fff;
+          border-radius: 4px;
+          margin-top: 10px;
+          text-decoration: none;
+          font-weight: 600;
+          color: #111;
+          opacity: 0;
+          transform: translateY(50px);
+          transition: 0.5s;
+        }
+      }
+
+      &:hover .contentBx .size,
+      &:hover .contentBx .color {
+        opacity: 1;
+        visibility: visible;
+        transition-delay: 0.6s;
+      }
+
+      &:hover .contentBx a {
+        opacity: 1;
+        transform: translateY(0px);
+        transition-delay: 0.75s;
+      }
+    }
+  }
+`;
