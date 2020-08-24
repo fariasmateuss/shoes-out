190b5b14ac6c21e8b2ec42111bfef314e32a65c3
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-24 00:01:13 -0400

    :sparkles: Initial checkout cart layout

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,7 +1,70 @@
 import React from 'react';
 
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
 function Cart() {
-  return <h1>Hello, World</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr />
+          <th>Produto</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+          <tr />
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
+            </td>
+
+            <td>
+              <strong>Shoe Vans Classic</strong>
+              <span>R$ 129,90</span>
+            </td>
+
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#fff" />
+                </button>
+                <input type="number" readOnly value={3} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#fff" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$ 389,70</strong>
+            </td>
+
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#fff" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$ 389,70</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 export default Cart;
