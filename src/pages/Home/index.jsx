800ed97c46771c642b73ca3fa1de482de2648a58
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-23 23:12:50 -0400

    :sparkles: Changed shoe nome

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,7 +11,57 @@ function Home() {
             <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
           </div>
           <div className="contentBx">
-            <h2>Vans Shoe</h2>
+            <h2>Vans Classic</h2>
+            <div className="size">
+              <h3>Size :</h3>
+              <span>37</span>
+              <span>38</span>
+              <span>39</span>
+              <span>40</span>
+            </div>
+            <div className="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <a href="#">Buy Now</a>
+          </div>
+        </div>
+      </li>
+
+      <li className="container">
+        <div className="card">
+          <div className="imgBx">
+            <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
+          </div>
+          <div className="contentBx">
+            <h2>Vans Classic</h2>
+            <div className="size">
+              <h3>Size :</h3>
+              <span>37</span>
+              <span>38</span>
+              <span>39</span>
+              <span>40</span>
+            </div>
+            <div className="color">
+              <h3>Color :</h3>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <a href="#">Buy Now</a>
+          </div>
+        </div>
+      </li>
+
+      <li className="container">
+        <div className="card">
+          <div className="imgBx">
+            <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
+          </div>
+          <div className="contentBx">
+            <h2>Vans Classic</h2>
             <div className="size">
               <h3>Size :</h3>
               <span>37</span>
