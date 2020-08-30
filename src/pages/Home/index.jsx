d2b24bf4bb7b34220e02ef6cd48097efb7c03056
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-08-29 20:23:28 -0400

    :sparkles: Fetching API data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,86 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
-function Home() {
-  return (
-    <ProductList>
-      <li className="container">
-        <div className="card">
-          <div className="imgBx">
-            <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
-          </div>
-          <div className="contentBx">
-            <h2>Vans Classic</h2>
-            <div className="size">
-              <h3>Size :</h3>
-              <span>37</span>
-              <span>38</span>
-              <span>39</span>
-              <span>40</span>
-            </div>
-            <div className="color">
-              <h3>Color :</h3>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <a href="#">Buy Now</a>
-          </div>
-        </div>
-      </li>
+class Home extends Component {
+  state = {
+    products: [],
+  };
 
-      <li className="container">
-        <div className="card">
-          <div className="imgBx">
-            <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
-          </div>
-          <div className="contentBx">
-            <h2>Vans Classic</h2>
-            <div className="size">
-              <h3>Size :</h3>
-              <span>37</span>
-              <span>38</span>
-              <span>39</span>
-              <span>40</span>
-            </div>
-            <div className="color">
-              <h3>Color :</h3>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <a href="#">Buy Now</a>
-          </div>
-        </div>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-      <li className="container">
-        <div className="card">
-          <div className="imgBx">
-            <img src="https://i.ibb.co/10hBqpX/vans.png" alt="Vans" />
-          </div>
-          <div className="contentBx">
-            <h2>Vans Classic</h2>
-            <div className="size">
-              <h3>Size :</h3>
-              <span>37</span>
-              <span>38</span>
-              <span>39</span>
-              <span>40</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id} className="container">
+            <div className="card">
+              <div className="imgBx">
+                <img src={product.image} alt={product.title} />
+              </div>
+              <div className="contentBx">
+                <h2>{product.title}</h2>
+                <div className="size">
+                  <h3>Size :</h3>
+                  <span>37</span>
+                  <span>38</span>
+                  <span>39</span>
+                  <span>40</span>
+                </div>
+                <div className="color">
+                  <h3>Color :</h3>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+                <div className="price">
+                  <h3>Price :</h3>
+                  <span>{product.priceFormatted}</span>
+                </div>
+                <button type="button">Buy Now</button>
+              </div>
             </div>
-            <div className="color">
-              <h3>Color :</h3>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <a href="#">Buy Now</a>
-          </div>
-        </div>
-      </li>
-    </ProductList>
-  );
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
 
 export default Home;
