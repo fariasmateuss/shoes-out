d335fc0cd210ab6c152ba855d36ef8c160de909f
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-09-02 00:04:03 -0400

    :sparkles: Increment and decrement of the quantity in the cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -24,6 +24,20 @@ function cart(state = [], action) {
           draft.splice(productIndex, 1);
         }
       });
+
+    case '@cart/UPDATE_AMOUNT':
+      if (action.amount <= 0) {
+        return state;
+      }
+
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+
     default:
       return state;
   }
