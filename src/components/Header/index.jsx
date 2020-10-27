2498831a3c1fb6eca650c6c832a4a65def5f7b8e
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-10-27 07:43:07 -0400

    :sparkles: Destruct data property title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import { MdShoppingCart } from 'react-icons/md';
 import { Container, Cart } from './styles';
 
-import { MdShoppingCart } from 'react-icons/md';
-
-function Header(props) {
+function Header({ title }) {
   const cartSize = useSelector(state => state.cart.length);
 
   return (
     <Container>
       <Link to="/">
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
       </Link>
 
       <Cart to="/cart">
