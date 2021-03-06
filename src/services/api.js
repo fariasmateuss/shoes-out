65a380d4699bcfbfc9f07662d4fa6bf377a79891
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-03-06 00:45:24 -0400

    :sparkles: Add API connection key

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://shoes-out.herokuapp.com',
+  baseURL: process.env.REACT_APP || 'http://localhost:3333/',
 });
 
 export default api;
