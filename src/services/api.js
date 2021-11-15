a1c17a00f304b747d174a701a8936ef01456898e
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-11-15 00:41:17 -0400

    :bug: Update base url to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP || 'http://localhost:3333/',
+  baseURL: 'https://shoes-out.herokuapp.com',
 });
 
 export default api;
